d98621c4c8baffc4b38ee6d05a0634c1593ede55
T-107 <aomeval@live.com.mx>
2019-02-01 21:27:00 -0600

    otra vez

---
+++ app/src/main/java/unitec/org/a1/ElementosAndoird.java
@@ -0,0 +1,13 @@
+package unitec.org.a1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ElementosAndoird extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_elementos_andoird);
+    }
+}
